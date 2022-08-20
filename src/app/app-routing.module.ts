fdfac4f94abb664aaba41913cd3fc1af60c46cca
Lucas Oliveira <lucasp.oliveira122@gmail.com>
2022-08-20 17:21:06 -0300

    rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
@@ -11,6 +12,9 @@ const routes: Routes = [
   ,
   {
     path : "login", component : LoginComponent
+  },
+  {
+    path : "carousel" , component : CarouselComponent
   }
 ];
 
