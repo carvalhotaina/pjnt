423f881b13d7b90f3ea9da489ab88e09413da389
Lucas Oliveira <lucasp.oliveira122@gmail.com>
2022-08-20 15:12:02 -0300

    Atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,11 @@ export class AppComponent {
        //'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
       imageAlt: 'nature1',
     },
+    {
+      imageSrc:
+        'https://cdn.pegadesconto.com.br/logotipo-pichau-informatica-1.png',
+      imageAlt: 'nature2',
+    },
     
   ]
   
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -14,12 +14,45 @@ export class CarouselComponent implements OnInit {
 
   @Input() imagens: corouselImagens[] = []
   @Input() indicators = true;
+  @Input() controls = true;
+  @Input() autoSlide = false;
+  @Input() slideInterval = 3000; ///Default 3 segundos
 
   selectedIndex = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    if(this.autoSlide){
+      this.autoSlideImagens();
+    }
+  }
+
+  autoSlideImagens(): void{
+    setInterval(() => {
+      this.onNextClick();
+    }, this.slideInterval);
+  }
+
+  selectImagens(index: number) : void{
+    this.selectedIndex = index;
+  }
+
+  onPrevClick():void{
+      if(this.selectedIndex === 0){
+        this.selectedIndex = this.imagens.length -1;
+      } else{
+        this.selectedIndex--;
+      }
+
+  }
+
+  onNextClick():void{
+    if(this.selectedIndex === this.imagens.length -1){
+      this.selectedIndex = 0;
+    } else{
+      this.selectedIndex++;
+    }
   }
 
 }
